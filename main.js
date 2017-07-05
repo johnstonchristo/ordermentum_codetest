e8cacaf87023893bec07ee325f34fc30f5f6acee
johnstonchristo <johnstonchristo@gmail.com>
2017-07-05 20:40:00 +1000

    third commit

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log("code test");
-
 var access_light = function(lights_color, background_color) {
   var light = document.getElementById(lights_color);
   light.style.backgroundColor = background_color;
@@ -13,37 +11,37 @@ var start_and_finish_time = function(start_time, finish_time) {
   return duration;
 };
 
-var timer = function(lights_color, background_color, milliseconds) {
-  setTimeout(function() {
-    access_light(lights_color, background_color);
-  }, milliseconds);
-};
-
-var minutes = function(minutes) {
+var time_conversion = function(minutes) {
   var new_time = minutes * 60000;
   return new_time;
 };
 
-var e_w = function(seconds) {
-  timer("ew_green", "green", 0);
-  timer("ew_green", "white", minutes(4.5));
-  timer("ew_yellow", "yellow", minutes(4.5));
-  timer("ew_yellow", "white", minutes(5));
-  timer("ew_red", "red", minutes(5));
-  timer("ew_red", "white", minutes(9.5));
-  timer("ew_yellow", "yellow", minutes(9.5));
-  timer("ew_yellow", "white", minutes(10));
+var timer = function(lights_color, background_color, minutes) {
+  setTimeout(function() {
+    access_light(lights_color, background_color);
+  }, time_conversion(minutes));
 };
 
-var n_s = function(seconds) {
+var e_w = function() {
+  timer("ew_green", "green", 0);
+  timer("ew_green", "white", 4.5);
+  timer("ew_yellow", "yellow", 4.5);
+  timer("ew_yellow", "white", 5);
+  timer("ew_red", "red", 5);
+  timer("ew_red", "white", 9.5);
+  timer("ew_yellow", "yellow", 9.5);
+  timer("ew_yellow", "white", 10);
+};
+
+var n_s = function() {
   timer("ns_red", "red", 0);
-  timer("ns_red", "white", minutes(4.5));
-  timer("ns_yellow", "yellow", minutes(4.5));
-  timer("ns_yellow", "white", minutes(5));
-  timer("ns_green", "green", minutes(5));
-  timer("ns_green", "white", minutes(9.5));
-  timer("ns_yellow", "yellow", minutes(9.5));
-  timer("ns_yellow", "white", minutes(10));
+  timer("ns_red", "white", 4.5);
+  timer("ns_yellow", "yellow", 4.5);
+  timer("ns_yellow", "white", 5);
+  timer("ns_green", "green", 5);
+  timer("ns_green", "white", 9.5);
+  timer("ns_yellow", "yellow", 9.5);
+  timer("ns_yellow", "white", 10);
 };
 
 var both_lights = function() {
@@ -55,10 +53,10 @@ var run_lights = function(start_time, finish_time) {
   both_lights();
   setInterval(function() {
     both_lights();
-  }, minutes(10));
+  }, time_conversion(10));
   setTimeout(function() {
     clearInterval(both_lights);
-  }, minutes(start_and_finish_time()));
+  }, time_conversion(start_and_finish_time(start_time, finish_time)));
 };
 
 run_lights("January 1, 2017 9:30:00", "January 1, 2017 10:00:00");
